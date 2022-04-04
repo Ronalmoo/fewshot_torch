8ac0c0227a5626e791d3fe5bddc3aea12e3b41c7
Seung-Moo Yang <44221520+Ronalmoo@users.noreply.github.com>
2022-04-04 14:04:24 +0900

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_graphs(history, string):
-    plt.plot(history.history[string])
-    plt.plot(history.history['val_'+string], '')
-    plt.xlabel('Epochs')
-    plt.ylabel(string)
-    plt.legend([string, 'val_'+string])
-    plt.show()
